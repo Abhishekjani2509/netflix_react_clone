2daed8f354caa8f76ba6e594c1519b7d345c8872
Abhishekjani2509 <abhishekjani2509@gmail.com>
2023-03-07 01:07:03 +0530

    2nd commit

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const key= "c44db6ea0669d56ee15e16997763d72f"
+
+const requests={
+    requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror:`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestUpcoming:`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
+    
+    
+}
+export default requests
